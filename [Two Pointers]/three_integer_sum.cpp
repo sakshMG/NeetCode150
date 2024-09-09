877500119cdc932abec4f0283d97c24dca21f4da
Saksham Gupta <saksham.2002.gupta@gmail.com>
2024-09-10 00:41:31 +1000

    update

--- [Two Pointers]/three_integer_sum.cpp
+++ [Two Pointers]/three_integer_sum.cpp
@@ -8,7 +8,36 @@ class Solution {
 
             vector<vector<int>> result; 
 
+            sort(nums.begin(), nums.end());
 
+            for (int i = 0; i < nums.size(); i++) {
+                
+                if (nums[i] > 0) break; 
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                int l = i + 1;
+                int r = nums.size() - 1; 
+
+                while (l < r) {
+                    int value = nums[i] + nums[l] + nums[r];
+
+                    if (value == 0) {
+                        
+                        result.push_back({nums[i], nums[l], nums[r]});
+                        l++;
+                        r--;
+                        
+                        while (l < r && nums[l] == nums[l - 1]) {
+                            l++; 
+                        }
+                        
+                    } else if (value < 0) {
+                        l++;
+                    } else if (value > 0) {
+                        r--;
+                    }
+                }
+            }
 
             return result;
         }
